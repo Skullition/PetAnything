c40473d6624ebd071f1825feec3ed3830f2b6346
skullition <78258797+Skullition@users.noreply.github.com>
2021-10-13 20:36:00 +0700

    Create EntityMixin

---
+++ src/main/java/skullition/petanything/PetAnything.java
@@ -0,0 +1,13 @@
+package skullition.petanything;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PetAnything implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("petanything");
+
+	@Override
+	public void onInitialize() {
+	}
+}
